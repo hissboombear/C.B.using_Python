ac28315ca433a99810337ea3e1f657878e2afc63
hissboombear <hissboombear@gmail.com>
2018-01-14 09:33:12 -0500

    billy.py

---
+++ billy.py
@@ -0,0 +1,99 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 18:41:36) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 2 + 2
+4
+>>> 2 - 2
+0
+>>> 2 * 2
+4
+>>> 2 / 2
+1.0
+>>> bananas = 5
+>>> bananaseaten =2
+>>> bananas - bananaseaten
+3
+>>> 
+>>> bananas = 10
+>>> bananaseaten = 4
+>>> bananas - bananaseaten
+6
+>>> 
+>>> exampleString = "I am a string"
+>>> 
+>>> name = imput("What is your name")
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    name = imput("What is your name")
+NameError: name 'imput' is not defined
+>>> name = imput("What is your name?")
+Traceback (most recent call last):
+  File "<pyshell#15>", line 1, in <module>
+    name = imput("What is your name?")
+NameError: name 'imput' is not defined
+>>> name = input("What is your name?")
+What is your name?
+>>> print(name)
+
+>>> print("plerf")
+plerf
+>>> 
+[DEBUG ON]
+>>> 
+[DEBUG OFF]
+>>> print("Create your charachter!")
+Create your charachter!
+>>> name = input("What is your charachter called?")
+What is your charachter called?
+>>> input("hoerswgywkurgtwi")
+hoerswgywkurgtwiinput("hoerswgywkurgtwi")
+'input("hoerswgywkurgtwi")'
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+
+
+
+>>> 
+>>> 
+
+
+
+>>> 
+>>> 
+
+
+
+>>> 
+>>> 
+
+
+>>> 
+>>> 
+
+
+>>> input ("How old is your charachter")
+How old is your charachter
+''
+>>> print("Create your charachter!")
+Create your charachter!
+>>> name = input("What is your character called?")
+What is your character called?("Brian")
+>>> age = input("How old is your charachter?")
+How old is your charachter?("245")
+>>> strengths = input("What are your charachters strengths?")
+What are your charachters strengths?("Can fly")
+>>> weaknesses = input("What are your charachters weaknesses?)
+		   
+SyntaxError: EOL while scanning string literal
+>>> weaknesses = input("What are your charachters weaknesses?")
+What are your charachters weaknesses?("Tells terrible jokes")
+>>> 
+>>> 
+>>> 
+>>> print("Your charachter's name is": name)																		print("Your charachter is" age, "years old")																		print("Strengths:", strengths)																				print("Weaknesses:", weaknesses)																			print(name, "says, 'Thanks for creating me!'")
+SyntaxError: invalid syntax
+>>> print("Your charachter's name is", name)																		print("Your charachter is", age, "years old")																		print("Strengths:", strengths)																				print("Weaknesses:", weaknesses)																			print(name, "says 'Thanks for creating me!'")
+SyntaxError: invalid syntax
+>>> 
