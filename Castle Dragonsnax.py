0165b9a1b6ef7c47c6230d358002edb583135fee
hissboombear <hissboombear@gmail.com>
2018-01-20 13:39:57 -0500

    Updating C.B.

---
+++ Castle Dragonsnax.py
@@ -0,0 +1,24 @@
+print("You are in a dark room in the mysterious Castle Dragonsnax.")
+print("In front of you are four doors. You must choose one.")
+playerChoice = input("Choose 1, 2 or 3... ")
+if playerChoice == "1":
+    print("You find a room full of treasure. You're dissapointed.")
+    print("GAME OVER, YOU WIN!")
+elif playerChoice == "2":
+    print("The door opens and an angry ogre hits you with his club")
+    print("GAME OVER YOU LOSE!")
+elif playerChoice == "3":
+    print("You go into the room and find a sleeping dragon.")
+    print("1) Try to steal some of the dragon's gold.")
+    print("2) Try to sneak around the dragon to the exit.")
+    dragonChoice = input("Type 1 or 2...")
+    if dragonChoice == "1":
+        print("Debugging")
+    elif dragonChoice == "2":
+      print("You sneak around klxjvk mnvxn xbvkcmx,xmx,fdnjfdcnx,n dj,cmsfjdfn ,dfnvcksjfn sjncsm,z sjnckx, scmnkm,xc.") 
+      print("The dragon wakes up and eats you. You are delicious ... and that's why they call it Castle Dragosnax.")
+      print("GAME OVER YOU LOSE")
+      
+    
+    
+    
