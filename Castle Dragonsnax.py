8feeaee429ccb9de1cdf61f020db70c2e3fbca1b
hissboombear <hissboombear@gmail.com>
2018-02-03 11:24:46 -0500

    Happy

--- Castle Dragonsnax.py
+++ Castle Dragonsnax.py
@@ -1,8 +1,8 @@
 print("You are in a dark room in the mysterious Castle Dragonsnax.")
-print("In front of you are four doors. You must choose one.")
-playerChoice = input("Choose 1, 2 or 3... ")
+print("In front of you are three doors. You must choose one.")
+playerChoice = input("Choose 1, 2 or 3 ... ")
 if playerChoice == "1":
-    print("You find a room full of treasure. You're dissapointed.")
+    print("You find a room full of treasure. You're rich!")
     print("GAME OVER, YOU WIN!")
 elif playerChoice == "2":
     print("The door opens and an angry ogre hits you with his club")
@@ -13,12 +13,16 @@ elif playerChoice == "3":
     print("2) Try to sneak around the dragon to the exit.")
     dragonChoice = input("Type 1 or 2...")
     if dragonChoice == "1":
-        print("Debugging")
+        print("The dragon wakes up and eats you. You are delicious.")
+        print("GAME OVER, YOU LOSE.")
     elif dragonChoice == "2":
-      print("You sneak around klxjvk mnvxn xbvkcmx,xmx,fdnjfdcnx,n dj,cmsfjdfn ,dfnvcksjfn sjncsm,z sjnckx, scmnkm,xc.") 
-      print("The dragon wakes up and eats you. You are delicious ... and that's why they call it Castle Dragosnax.")
-      print("GAME OVER YOU LOSE")
-      
+        print("You sneak around the dragon and escape the castle, blinking in the sunshine.")
+        print("GAME OVER, YOU WIN")
+    else:
+        print("Sorry, you didn't enter 1 or 2!")
+else:
+    print("Sorry you didn't enter 1, 2 or 3!")
+print("Run the game again and have another go.")
     
     
     
