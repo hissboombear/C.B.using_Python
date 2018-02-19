a03fad74509d4c7b1537958891e2a8255864b345
hissboombear <hissboombear@gmail.com>
2018-02-19 17:49:32 -0500

    Debbuging

---
+++ bomber.py
@@ -0,0 +1,27 @@
+import tkinter
+import random
+gameOver = False
+score = 0
+squaresToClear = 0
+def play_bombdoger():
+    create_bombfield(bombfield)
+    window = tkinter.Tk()
+    layout_window(window)
+    window.mainloop()
+bombfield = []
+def create_bombfield(bombfield):
+    global squaresToClear
+    for row in range(0,10):
+        rowList = []
+        for column in range(0,10):
+            if random.randit(1,100) < 20:
+                rowList.append(1)
+            else:
+                rowList.append(0)
+                squaresToClear = squaresToClear + 1
+        bombfield.append(rowList)
+    printfield(bombfield)
+def printfield(bombfield):
+    for rowList in bombfield:
+        print(rowList)
+play_bombdoger()
